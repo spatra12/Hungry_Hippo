4f6854be35bce6aacc9bf1a1c3cb2745aec12eae
spatra12 <samarpita.patra@gmail.com>
2016-06-05 15:31:12 -0700

    Locatable update

--- finalproject/src/finalproject/Locatable.java
+++ finalproject/src/finalproject/Locatable.java
@@ -1,5 +1,11 @@
 package finalproject;
 
-public class Locatable {
+public interface Locatable {
 
+	public void setPos( int x, int y);
+    public void setX( int x );
+    public void setY( int y );
+
+    public int getX();
+    public int getY();
 }
