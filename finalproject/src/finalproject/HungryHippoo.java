3659185e030fdcb3d5a32385b97584910c465564
spatra12 <samarpita.patra@gmail.com>
2016-06-05 17:28:13 -0700

    Made small changes to Starfighter structure

--- finalproject/src/finalproject/HungryHippoo.java
+++ finalproject/src/finalproject/HungryHippoo.java
@@ -1,14 +1,28 @@
 package finalproject;
 
-public class HungryHippoo {
+import javax.swing.JFrame;
+import java.awt.Component;
+
+public class HungryHippoo extends JFrame{
 	
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+
 	public HungryHippoo()
 	{
-		
+		super("Hungry Hippo");
+		setSize(WIDTH,HEIGHT);
+
+		Wild theGame = new Wild();
+		((Component)theGame).setFocusable(true);
+
+		getContentPane().add(theGame);
+
+		setVisible(true);
 	}
 
 	public static void main(String args[])
 	{
-		
+		HungryHippoo run = new HungryHippoo(); 
 	}
 }
